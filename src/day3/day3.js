0ab1814245cb657ea41b24cd003689e1aa25b33a
gfteix <gabriel.fteixeira13@gmail.com>
2022-12-04 11:54:07 -0300

    day 3 part 2

--- src/day3/day3.js
+++ src/day3/day3.js
@@ -9,8 +9,14 @@ const alphaVal = (s) => {
     return (isUpperCase(s)) ? (val + 26) : val
 }
 
+const groupArrayByThree = (arr) => {
+    return Array.from({length: arr.length/3}, () => arr.splice(0,3));
+}
+
 const main = async function() {
-   const input = (await readFile('src/day3/input_day3.txt')).split('\n').map(item => {
+   const input = (await readFile('src/day3/input_day3.txt')).split('\n')
+   /* first part
+   const priorities = input.map(item => {
         const middle = item.length/2
         const firstHalf = item.substring(0, middle)
         const secondHalf = item.substring(middle)
@@ -27,8 +33,24 @@ const main = async function() {
             return sum += alphaVal(value)
         }, 0)
     })
+*/
 
-    const result = input.reduce((sum, value) => sum += value)
+  //second part
+  const groupedArray = groupArrayByThree(input)
+  const priorities = groupedArray.map((array) => {
+    const duplicated = new Set()
+    for(const i in array[0]) {
+        if(array[1].includes(array[0][i]) && array[2].includes(array[0][i])) {
+            duplicated.add(array[0][i])
+        }
+    }
+
+    return [...duplicated].reduce((sum, value) => sum += alphaVal(value), 0)
+  })
+
+
+    const prioritiesSum = priorities.reduce((sum, value) => sum += value, 0)
+    console.log(prioritiesSum)
 }
 
 main()
